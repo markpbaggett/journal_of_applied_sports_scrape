aa77551495eaa9856843ee75e9c4ac6995fc8c4a
Mark Baggett <mbagget1@utk.edu>
2020-10-14 10:57:43 -0400

    Download our PDFs.

---
+++ download.py
@@ -0,0 +1,7 @@
+import requests
+
+with open("download.txt", 'r') as downloads:
+    for download in downloads:
+        r = requests.get(download.replace("\n", ""))
+        with open(f'downloads/{download.replace("https://js.sagamorepub.com/jasm/article/download/", "").replace("/", "_").strip()}.pdf', 'wb') as current:
+            current.write(r.content)
